7dd1235ee2e9187c73a328a5fe60474b7315b7dd
Richard Tae <taerichard@gmail.com>
2022-10-12 22:01:32 -0700

    installed emailjs module, attached events to inputs, created submit function to send email with error catch, working on displaying success and error page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/contactPageStyles.css";
 import ContactSection from "../components/work/Contact";
 import { send } from "emailjs-com";
@@ -7,28 +7,66 @@ import { send } from "emailjs-com";
 
 // https://www.youtube.com/watch?v=aor9hlcODUE&ab_channel=EricMurphy
 function ContactPage() {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send("service_32vtkct", "template_5hbrrwb", toSend, "JhOgiaEdWfT0fpG2Q")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="contactPage-container">
       <h1 className="contactPage-title">
         Contact me for any questions.
         <br /> What can I do for you?
       </h1>
-      <form className="contactPage-form">
+
+      <form className="contactPage-form" onSubmit={onSubmit}>
         <div className="form-nameEmail">
           <div className="name-container">
             <label className="nameLabel" for="name">
               Name
             </label>
-            <input name="name" type="text" />
+            <input
+              name="name"
+              type="text"
+              // value={toSend.from_name}
+              // onChange={handleChange}
+            />
           </div>
           <div className="email-container">
             <label for="email">Email</label>
-            <input name="email" type="text" />
+            <input
+              name="from_name"
+              type="text"
+              value={toSend.from_email}
+              onChange={handleChange}
+            />
           </div>
         </div>
         <div className="form-msgBtn">
           <label for="message">Message</label>
-          <textarea name="message" type="text"></textarea>
+          <textarea
+            name="message"
+            type="text"
+            value={toSend.message}
+            onChange={handleChange}
+          ></textarea>
           <button>Submit</button>
         </div>
       </form>
